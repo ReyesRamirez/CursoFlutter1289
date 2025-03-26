1583eaa5970ce038afd23fff7dae3b292b12d780
REYES_ALFONSO_RAMIREZ_MARTINEZ <a.22308051281289@cbtis128.edu.mx>
2025-03-26 19:27:59 +0000

    ahora si

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:ramirezcurso/mi_widgets/mi_tarjeta.dart';
+
+import 'package:ramirezcurso/mi_widgets/dialogo_acerca_de.dart';
+//import 'package:ramirezcurso/mi_widgets/mi_tarjeta.dart';
 
 const Color darkBlue = Color(0xFF12202F);
 void main() => runApp(MiTarjeta());
@@ -18,7 +20,8 @@ class MiTarjeta extends StatelessWidget {
             title: const Text("Flutter Ramirez"),
             centerTitle: true,
           ),
-          body: const LaTarjeta(),
+          //body: const LaTarjeta(),
+          body: const DialogoAcercaDe(),
         ));
   }
 }
--- z_recursos/leeme.md
+++ z_recursos/leeme.md
@@ -1,2 +1,6 @@
 ## Ejemplo Card
 ![La tarjeta](Card.jpg)
+
+## Ejemplo Un Dialogo de Alerta
+![La tarjeta](dialogo.jpg)
+![La tarjeta](dialogor.jpg)
